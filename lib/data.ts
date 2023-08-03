5784e90696ac4378b2d8bbd49a4b1d759604ce5c
westfalensgod <westfalensgod@gmail.com>
2023-08-03 20:37:59 +0200

    chore: add an article and update information about one of the projects

--- lib/data.ts
+++ lib/data.ts
@@ -4,11 +4,18 @@ export interface Article {
     link: string
 }
 
-export const articles: Article[] = [{
-    id: 1,
-    title: "Memoization in React",
-    link: "https://medium.com/@yanisneverlies/leveling-up-practices-every-react-developer-should-know-memoization-80cf693f456d"
-}]
+export const articles: Article[] = [
+    {
+        id: 1,
+        title: "Memoization in React",
+        link: "https://medium.com/@yanisneverlies/leveling-up-practices-every-react-developer-should-know-memoization-80cf693f456d"
+    },
+    {
+        id: 2,
+        title: "Integrate TickTick with Fantastical using Zapier",
+        link: "https://medium.com/@yanisneverlies/how-to-integrate-ticktick-with-fantastical-using-zapier-61ffc585d4c4",
+    }
+]
 
 export interface Project {
     id: number
@@ -19,9 +26,9 @@ export interface Project {
 
 export const projects: Project[] = [{
     id: 1,
-    title: "🚧 React patterns",
+    title: "🚧 Frontend Notes",
     link: "#",
-    description: "A database with infographics about different React patterns with production examples"
+    description: "A database with notes about different Frontend patterns with production examples"
 }]
 
 // Rename place
