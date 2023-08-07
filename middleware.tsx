b88203cc73fe2c056acd596622d5afb8bc3f7705
westfalensgod <westfalensgod@gmail.com>
2023-08-08 00:19:12 +0200

    fix: temporarily disable rewrites for notes route

--- middleware.tsx
+++ middleware.tsx
@@ -14,26 +14,26 @@ export const config = {
 };
 
 export default async function middleware(req: NextRequest) {
-    const url = req.nextUrl;
-
-    const hostname = req.headers
-        .get("host")!
-        .replace(".localhost:3001", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-
-    const path = url.pathname;
-
-    if (hostname == `notes.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
-        return NextResponse.rewrite(
-            new URL(`/notes${path === "/" ? "" : path}`, req.url),
-        );
-    }
-
-    if (
-        hostname === "localhost:3001" ||
-        hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-    ) {
-        return NextResponse.rewrite(new URL(req.url));
-    }
-
-    return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
+    // const url = req.nextUrl;
+    //
+    // const hostname = req.headers
+    //     .get("host")!
+    //     .replace(".localhost:3001", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+    //
+    // const path = url.pathname;
+    //
+    // if (hostname == `notes.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+    //     return NextResponse.rewrite(
+    //         new URL(`/notes${path === "/" ? "" : path}`, req.url),
+    //     );
+    // }
+    //
+    // if (
+    //     hostname === "localhost:3001" ||
+    //     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
+    // ) {
+    //     return NextResponse.rewrite(new URL(req.url));
+    // }
+    //
+    // return NextResponse.rewrite(new URL(`/${hostname}${path}`, req.url));
 }
